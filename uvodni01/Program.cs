655e650a380059e9a0a88a82347a3f93012d6117
Lovro Šverko <lovro.sverko@gmail.com>
2023-09-20 08:42:55 +0200

    Add files via upload

---
+++ uvodni01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodni01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, x, y;
+            a = 100;
+            b = 3;
+            x = a / b;
+            y = a % b;
+
+            Console.WriteLine(a + " dijeljeno sa " + b +
+                " je " + x + " s ostatkom " + y);
+
+
+            Console.ReadKey();
+        }
+    }
+}
